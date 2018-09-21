4eb1237b8773f774dddb5e8c7010869eeeea5d51
KidPluto <tigers-02121@pobox.com>
2018-09-21 19:19:15 -0400

    New and small changs

--- src/net/kidpluto/RomanNumeralsConverter/test/testConvertToRomanNumerals.java
+++ src/net/kidpluto/RomanNumeralsConverter/test/testConvertToRomanNumerals.java
@@ -3,8 +3,10 @@ package net.kidpluto.RomanNumeralsConverter.test;
 import net.kidpluto.RomanNumeralsConverter.ConvertToRomanNumerals;
 import net.kidpluto.RomanNumeralsConverter.NumberAndRomanNumeralPairsList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.Test;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class testConvertToRomanNumerals {
 
